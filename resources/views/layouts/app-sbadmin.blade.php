dbcb9aca4ceef3263a65f3832d42b300ec866d48
Charly1209 <charlyponce9@gmail.com>
2020-03-03 01:05:26 -0600

    First version of Frontend

---
+++ resources/views/layouts/app-sbadmin.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="sb-nav-fixed">
+
+  @includeIf('include.navbar')
+  
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+      @includeIf('include.sidebar')
+    </div>
+
+    <div id="layoutSidenav_content">
+      <main>
+        @yield('content')
+      </main>
+      @includeIf('include.footer')
+    </div>
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ asset('js/SBAdmin.js') }}" defer></script>
+</body>
+</html>
