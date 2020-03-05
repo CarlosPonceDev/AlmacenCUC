57df0dfda818e0399cc3133060aacd1be00b58b1
Charly1209 <charlyponce9@gmail.com>
2020-03-05 00:26:25 -0600

    Login with username

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -17,9 +17,12 @@
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-        <a class="dropdown-item" href="#">Settings</a><a class="dropdown-item" href="#">Activity Log</a>
+        <a class="dropdown-item" href="#">Settings</a>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="login.html">Logout</a>
+        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Cerrar sesión</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
       </div>
     </li>
   </ul>
