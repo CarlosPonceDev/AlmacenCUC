1f98ad8bcc6ce14b1389093a6ef572b0925c3111
Charly1209 <charlyponce9@gmail.com>
2020-03-16 01:29:19 -0600

    Sidebar username display added

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -41,7 +41,7 @@
     </div>
   </div>
   <div class="sb-sidenav-footer">
-    <div class="small">Logged in as:</div>
-    Start Bootstrap
+    <div class="small">Sesión iniciada como:</div>
+    <span class="text-light">{{ auth()->user()->username }}</span>
   </div>
 </nav>
