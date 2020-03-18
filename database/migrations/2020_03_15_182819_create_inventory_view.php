fc0076d55594dc102c258aee4e6eba0d69121d3a
Charly1209 <charlyponce9@gmail.com>
2020-03-18 09:14:03 -0600

    Views update: Now it ignores the deleted rows

--- database/migrations/2020_03_15_182819_create_inventory_view.php
+++ database/migrations/2020_03_15_182819_create_inventory_view.php
@@ -42,15 +42,17 @@ class CreateInventoryView extends Migration
                     FROM entries en
                     WHERE en.product_id = p.id
                         AND en.place_id = (
-                        SELECT pl.id
-                        FROM places pl
-                        WHERE name LIKE '%cuc%'
-                    )
+                            SELECT pl.id
+                            FROM places pl
+                            WHERE name LIKE '%cuc%'
+                        )
+                        AND en.deleted_at IS NULL
                 ) -
                 (
                     SELECT IF(IFNULL(SUM(ex.quantity), 0) = 0, 0, SUM(ex.quantity))
                     FROM exits ex
                     WHERE ex.product_id = p.id
+                        AND ex.deleted_at IS NULL
                 )
             ) AS cuc,
             (
@@ -59,20 +61,22 @@ class CreateInventoryView extends Migration
                     FROM entries en
                     WHERE en.product_id = p.id
                         AND en.place_id = (
-                        SELECT pl.id
-                        FROM places pl
-                        WHERE name LIKE '%tomatlan%'
-                    )
+                            SELECT pl.id
+                            FROM places pl
+                            WHERE name LIKE '%tomatlan%'
+                        )
+                        AND en.deleted_at IS NULL
                 ) +
                 (
                     SELECT IF(IFNULL(SUM(ex.quantity), 0) = 0, 0, SUM(ex.quantity))
                     FROM exits ex
                     WHERE ex.product_id = p.id
                         AND ex.place_id = (
-                        SELECT pl.id
-                        FROM places pl
-                        WHERE name LIKE '%tomatlan%'
-                    )
+                            SELECT pl.id
+                            FROM places pl
+                            WHERE name LIKE '%tomatlan%'
+                        )
+                        AND ex.deleted_at IS NULL
                 )
             ) AS tomatlan,
             (
@@ -81,20 +85,22 @@ class CreateInventoryView extends Migration
                     FROM entries en
                     WHERE en.product_id = p.id
                         AND en.place_id = (
-                        SELECT pl.id
-                        FROM places pl
-                        WHERE name LIKE '%estacion-groumet%'
-                    )
+                            SELECT pl.id
+                            FROM places pl
+                            WHERE name LIKE '%estacion-groumet%'
+                        )
+                        AND en.deleted_at IS NULL
                 ) +
                 (
                     SELECT IF(IFNULL(SUM(ex.quantity), 0) = 0, 0, SUM(ex.quantity))
                     FROM exits ex
                     WHERE ex.product_id = p.id
                         AND ex.place_id = (
-                        SELECT pl.id
-                        FROM places pl
-                        WHERE name LIKE '%estacion-groumet%'
-                    )
+                            SELECT pl.id
+                            FROM places pl
+                            WHERE name LIKE '%estacion-groumet%'
+                        )
+                        AND ex.deleted_at IS NULL
                 )
             ) AS gourmet,
             (
@@ -106,18 +112,21 @@ class CreateInventoryView extends Migration
                         FROM places pl
                         WHERE name LIKE '%cuc%'
                     )
+                    AND en.deleted_at IS NULL
             ) AS entries,
             (
                 SELECT IF(IFNULL(SUM(ex.quantity), 0) = 0, 0, SUM(ex.quantity))
                 FROM exits ex
                 WHERE ex.product_id = p.id
+                    AND ex.deleted_at IS NULL
             ) AS exits,
             (
                 SELECT i.initial_stock
                 FROM inventory i
                 WHERE i.product_id = p.id
             ) AS initial_stock
-        FROM products p)");
+        FROM products p
+        WHERE p.deleted_at IS NULL)");
     }
 
     /**
