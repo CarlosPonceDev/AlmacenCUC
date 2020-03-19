f4c6163abda798d652d34bdf141ad9fcd8bf5778
Charly1209 <charlyponce9@gmail.com>
2020-03-18 23:15:02 -0600

    Categories view updated: Now it only loop through the categories in the database

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
@@ -13,7 +14,8 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        return view('categories.index');
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
     }
 
     /**
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,87 +3,17 @@
 @section('content')
   <h1>Categorías</h1>
 
-  <div class="w-100 mt-4">
-    <div class="row">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'tools',
-          'name'  => 'Herramientas'
-        ])
-        @endcomponent
+  <div class="w-100">
+    @foreach ($categories as $category)
+      <div class="row mt-4">
+        <div class="col">
+          @component('categories.components.category-button', [
+            'icon'  => $category->icon,
+            'name'  => $category->description
+          ])
+          @endcomponent
+        </div>
       </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'broom',
-          'name'  => 'Limpieza'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'cog',
-          'name'  => 'General'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'toilet-paper',
-          'name'  => 'Fontanería'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'bolt',
-          'name'  => 'Electricidad'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'fan',
-          'name'  => 'Aires Acondicionados'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'oil-can',
-          'name'  => 'Jardinería'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'brush',
-          'name'  => 'Píntura'
-        ])
-        @endcomponent
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        @component('categories.components.category-button', [
-          'icon'  => 'people-carry',
-          'name'  => 'Servicio'
-        ])
-        @endcomponent
-      </div>
-    </div>
+    @endforeach
   </div>
 @endsection
