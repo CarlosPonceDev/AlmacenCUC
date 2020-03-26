3b09a7f53f1cba764636662f0a1ddd7dfdbfc265
Charly1209 <charlyponce9@gmail.com>
2020-03-25 22:03:08 -0600

    Copyright added to the footer

--- resources/views/layouts/app-auth.blade.php
+++ resources/views/layouts/app-auth.blade.php
@@ -25,13 +25,7 @@
       </main>
     </div>
     <div id="layoutAuthentication_footer">
-      <footer class="py-4 bg-light mt-auto">
-        <div class="container-fluid">
-          <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2019</div>
-          </div>
-        </div>
-      </footer>
+      @includeIf('include.footer')
     </div>
   </div>
 
